5df27fd6ac3adee6ebae0b337177ca0f8521d404
Erik Kundt <bitshift@posteo.org>
2018-07-11 00:08:41 +0200

    Fixes end-to-end test.

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -4364,7 +4364,7 @@ BOOST_AUTO_TEST_CASE(fixed_arrays_in_storage)
 			function setData(uint index, uint x, uint y) public { data[index].x = x; data[index].y = y; }
 			function getID(uint index) public returns (uint) { return ids[index]; }
 			function getData(uint index) public returns (uint x, uint y) { x = data[index].x; y = data[index].y; }
-			function getLengths() public returns (uint l1, uint l2) public { l1 = data.length; l2 = ids.length; }
+			function getLengths() public returns (uint l1, uint l2) { l1 = data.length; l2 = ids.length; }
 		}
 	)";
 	compileAndRun(sourceCode);
